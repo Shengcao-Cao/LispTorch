e49e29c21ddec671175d8e176573eaf59a9cbbde
Friedrich <490626598@qq.com>
2019-04-15 12:46:40 +0800

    Tail recursion optimization

--- main.py
+++ main.py
@@ -25,30 +25,36 @@ class Procedure(object):
 
 def eval(x, env):
     "Evaluate an expression in an environment."
-    if isinstance(x, Symbol):      # variable reference
-        return env.find(x)[x]
-    elif not isinstance(x, List):  # constant literal
-        return x
-    elif x[0] == 'quote':          # (quote exp)
-        (_, exp) = x
-        return exp
-    elif x[0] == 'if':             # (if test conseq alt)
-        (_, test, conseq, alt) = x
-        exp = (conseq if eval(test, env) else alt)
-        return eval(exp, env)
-    elif x[0] == 'define':         # (define var exp)
-        (_, var, exp) = x
-        env[var] = eval(exp, env)
-    elif x[0] == 'set!':           # (set! var exp)
-        (_, var, exp) = x
-        env.find(var)[var] = eval(exp, env)
-    elif x[0] == 'lambda':         # (lambda (var...) body)
-        (_, parms, body) = x
-        return Procedure(parms, body, env)
-    else:                          # (proc arg...)
-        proc = eval(x[0], env)
-        args = [eval(exp, env) for exp in x[1:]]
-        return proc(*args)
+    while True:
+        if isinstance(x, Symbol):      # variable reference
+            return env.find(x)[x]
+        elif not isinstance(x, List):  # constant literal
+            return x
+        elif x[0] == 'quote':          # (quote exp)
+            (_, exp) = x
+            return exp
+        elif x[0] == 'if':             # (if test conseq alt)
+            (_, test, conseq, alt) = x
+            x = (conseq if eval(test, env) else alt)
+        elif x[0] == 'define':         # (define var exp)
+            (_, var, exp) = x
+            env[var] = eval(exp, env)
+            return None
+        elif x[0] == 'set!':           # (set! var exp)
+            (_, var, exp) = x
+            env.find(var)[var] = eval(exp, env)
+            return None
+        elif x[0] == 'lambda':         # (lambda (var...) body)
+            (_, parms, body) = x
+            return Procedure(parms, body, env)
+        else:                          # (proc arg...)
+            proc = eval(x[0], env)
+            args = [eval(exp, env) for exp in x[1:]]
+            if isinstance(proc, Procedure):
+                x = proc.body
+                env = Env(proc.parms, args, proc.env)
+            else:
+                return proc(*args)
 
 if __name__ == '__main__':
     global_env = standard_env()
