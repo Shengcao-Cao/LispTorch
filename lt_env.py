edc5891d82673d8cddc4e15b3aa55ac7c17b5367
Zhiqing Sun <zhiqing@pku.edu.cn>
2019-06-02 22:16:11 +0800

    Revert "Merge branch 'master' of https://github.com/Friedrich1006/LispTorch"
    This reverts commit 2b037feb1acd3f5928014b7a74cab3fe536211c0, reversing
    changes made to 847d9a191eb2cd46080f8eeb3aa3b6f23b7895f9.

--- lt_env.py
+++ lt_env.py
@@ -40,33 +40,8 @@ def standard_env():
 class Env(dict):
     "An environment: a dict of {'var':val} pairs, with an outer Env."
     def __init__(self, parms=(), args=(), outer=None):
-        arg_dict = dict()
-        for parm in parms:
-            if ':' in parm:
-                p, a = parm.split(':')
-                arg_dict[p] = atom(a)
-        for arg, parm in zip(args, parms):
-            if type(arg) is tuple:
-                p, a = arg
-                arg_dict[p] = a
-            else:
-                if ':' in parm:
-                    p, a = parm.split(':')
-                    arg_dict[p] = arg
-                else:
-                    arg_dict[parm] = arg
-
-        for parm in parms:
-            if ':' in parm:
-                p, a = parm.split(':')
-            else:
-                p = parm
-            if p not in arg_dict:
-                raise Exception('Parameter %s not given.' % p)
-
-        self.update(arg_dict)
+        self.update(zip(parms, args))
         self.outer = outer
-
     def find(self, var):
         "Find the innermost Env where var appears."
         return self if (var in self) else self.outer.find(var)
